cb274ab9d8f938345e72fca1f6fcf334b3fa0b2a
bryan <bryanokeke@gmail.com>
2019-03-14 19:36:33 -0400

    strip tests

--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -6,30 +6,24 @@ contract('TestERC721Mintable', accounts => {
         beforeEach(async function () { 
             this.contract = await ERC721MintableComplete.new({from: accounts[0]});
 
-            for(var i = 0; i < 11; i++) {
-                await this.contract.mintWithTokenURI(accounts[i], i, i.toString());
-            }
+            // mint mulitple tokens
         })
 
         it('should return contract owner', async function () { 
-            let owner = await this.contract.owner(); 
-            assert.equal(owner, accounts[0]);
+            
         })
 
         it('should return total supply', async function () { 
-            let totalSupply = await this.contract.totalSupply().toNumber(); 
-            assert.equal(totalSupply, 10);
+            
         })
 
         it('should get token balance', async function () { 
-            let tokenBalance = await this.contract.balanceOf(accounts[0]).toNumber(); 
-            assert.equal(tokenBalance, 1);
+            
         })
 
-        // token uri should be complete i.e: 
+        // token uri should be complete i.e: 'https://api.cryptokitties.co/kitties/1'
         it('should return token uri', async function () { 
-            let uri = await this.contract.tokenURI(1); 
-            assert.equal(uri, 'https://api.cryptokitties.co/kitties/1');
+           
         })
     });
 })
