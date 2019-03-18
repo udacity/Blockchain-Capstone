a07f8756738a22690d4f79fa1e24bf8fa7284460
bryan <bryanokeke@gmail.com>
2019-03-18 13:55:33 -0400

    cleaned up test structure

--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -2,19 +2,14 @@ var ERC721MintableComplete = artifacts.require('ERC721MintableComplete');
 
 contract('TestERC721Mintable', accounts => {
 
+    const account_one = accounts[0];
+    const account_two = accounts[1];
+
     describe('match erc721 spec', function () {
         beforeEach(async function () { 
-            this.contract = await ERC721MintableComplete.new({from: accounts[0]});
+            this.contract = await ERC721MintableComplete.new({from: account_one});
 
-            // mint mulitple tokens
-        })
-
-        it('should fail when minting when address is not contract owner', async function () { 
-            
-        })
-
-        it('should return contract owner', async function () { 
-            
+            // TODO: mint multiple tokens
         })
 
         it('should return total supply', async function () { 
@@ -25,13 +20,28 @@ contract('TestERC721Mintable', accounts => {
             
         })
 
-        // token uri should be complete i.e: 'https://api.cryptokitties.co/kitties/1'
+        // token uri should be complete i.e: https://api.cryptokitties.co/kitties/1
         it('should return token uri', async function () { 
-           
+            
         })
 
-        it('should transfer token from one owner to another', async function () { 
-           
+        it('should safe transfer token from one owner to another', async function () { 
+            
         })
+    });
+
+    describe('have ownership properties', function () {
+        beforeEach(async function () { 
+            this.contract = await ERC721MintableComplete.new({from: account_one});
+        })
+
+        it('should fail when minting when address is not contract owner', async function () { 
+            
+        })
+
+        it('should return contract owner', async function () { 
+            
+        })
+
     });
 })
