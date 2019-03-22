8dcdf4aa15866fcc4e4134cc9bff07c701284a2e
bryan <bryanokeke@gmail.com>
2019-03-21 22:31:56 -0400

    tweaked instructions

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,37 +1,34 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-// import './ERC721Mintable.sol';
-
-
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
 
 
 
-// TODO define another contract named SolnSquareVerifier that inherits ERC721Mintable
+// TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
 
 
 
-  // TODO define a solutions struct that can hold index, address, and image
+// TODO define a solutions struct that can hold index, address, and image
 
 
-  // TODO define an array of the above struct
+// TODO define an array of the above struct
 
 
-  // TODO define a mapping to store unique solutions submitted
-
-
-
-  // TODO Create an event to emit when a solution is added
+// TODO define a mapping to store unique solutions submitted
 
 
 
-  // TODO Create a function to add the solutions to the array and emit the event
+// TODO Create an event to emit when a solution is added
 
 
 
-  // TODO Create a function to mint new NFT only after the solution has been verified
-  //  - make sure the solution is unique (has not been used before)
-  //  - make sure you handle metadata as well as tokenSuplly
+// TODO Create a function to add the solutions to the array and emit the event
+
+
+
+// TODO Create a function to mint new NFT only after the solution has been verified
+//  - make sure the solution is unique (has not been used before)
+//  - make sure you handle metadata as well as tokenSuplly
 
   
 
