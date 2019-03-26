8c1f2586a55eaf8e97b351670d30406a7ab41644
bryan <bryanokeke@gmail.com>
2019-03-26 12:51:31 +0000

    fix typos and add missing step to instruction

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -10,7 +10,7 @@ contract Ownable {
     //  TODO's
     //  1) create a private '_owner' variable of type address with a public getter function
     //  2) create an internal constructor that sets the _owner var to the creater of the contract 
-    //  3) create an 'onlyOwner' modifer that throws if called by any account other than the owner.
+    //  3) create an 'onlyOwner' modifier that throws if called by any account other than the owner.
     //  4) fill out the transferOwnership function
     //  5) create an event that emits anytime ownerShip is transfered (including in the constructor)
 
@@ -23,9 +23,10 @@ contract Ownable {
 
 //  TODO's: Create a Pausable contract that inherits from the Ownable contract
 //  1) create a private '_paused' variable of type bool
-//  2) create an internal constructor that sets the _paused variable to false
-//  3) create 'whenNotPaused' & 'paused' modifier that throws in the appropriate situation
-//  4) create a Paused & Unpaused event that emits the address that triggered the event
+//  2) create a public setter using the inherited onlyOwner modifier 
+//  3) create an internal constructor that sets the _paused variable to false
+//  4) create 'whenNotPaused' & 'paused' modifier that throws in the appropriate situation
+//  5) create a Paused & Unpaused event that emits the address that triggered the event
 
 contract ERC165 {
     bytes4 private constant _INTERFACE_ID_ERC165 = 0x01ffc9a7;
@@ -442,7 +443,7 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
 
 
     // TODO: Create an internal function to set the tokenURI of a specified tokenId
-    // It should be the _baseToeknURI + the tokenId in string form
+    // It should be the _baseTokenURI + the tokenId in string form
     // TIP #1: use strConcat() from the imported oraclizeAPI lib to set the complete token URI
     // TIP #2: you can also use uint2str() to convert a uint to a string
         // see https://github.com/oraclize/ethereum-api/blob/master/oraclizeAPI_0.5.sol for strConcat()
